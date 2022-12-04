d6ec29aa3b08ed42bba035625da40e396de8367b
tetianasabanina <tatiana08st@gmail.com>
2022-12-04 20:31:35 +0200

    Add JWT authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,33 @@ import { AddTodo } from "./components/AddTodo";
 import { TodoList } from "./components/TodoList";
 import { TodoStatus } from "./components/TodoStatus";
 
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import { Nav, PrivateRoute } from "./components";
+import { history } from "./_helpers";
+import { Home } from "./pages";
+import { Login } from "./pages";
+
 import "./App.css";
 
 const App = () => {
-  return (
-    <div>
-      <TodoStatus />
-      <AddTodo />
-      <TodoList />
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter history={history}>
+				<Nav />
+				<div className="container pt-4 pb-4">
+					<Routes>
+						<PrivateRoute exact path="/" component={<Home />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="*" element={<Navigate to="/" />} />
+						<Navigate to="/" />
+					</Routes>
+				</div>
+			</BrowserRouter>
+			<TodoStatus />
+			<AddTodo />
+			<TodoList />
+		</div>
+	);
 };
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import App from "./App";
 
+// setup fake backend;
+import { fakeBackend } from "./_helpers";
+fakeBackend();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </React.StrictMode>
+	<React.StrictMode>
+		<RecoilRoot>
+			<App />
+		</RecoilRoot>
+	</React.StrictMode>
 );
